07de923ee14b5177a6cfe068301d384b50eba540
Ken Zimny <kenneth.zimny@gmail.com>
2025-03-28 06:15:08 -0500

    Build on Centered Skills WIP

--- src/app/skills/page.js
+++ src/app/skills/page.js
@@ -4,7 +4,7 @@ import Skills from "@/components/skills/Skills";
 export default function skills() {
   return (
     <>
-      <PageHeader className="text-center" title="Proficiencies" />
+      <PageHeader className="text-center" title="Skills" />
 
       <Skills />
     </>

9d020618917337322c21a0af749751dc7c831405
Ken Zimny <kenneth.zimny@gmail.com>
2025-03-28 08:32:54 -0500

    WIP Skills center

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -20,7 +20,7 @@ export default function Skills() {
                 })}
               </ul>
             </section>
-            <hr />
+            {/* <hr /> */}
           </Fragment>
         );
       })}

fcbb9b4430936dee1747430f34d405486d00ac90
Ken Zimny <kenneth.zimny@gmail.com>
2025-04-12 18:41:14 -0500

    Removed ul nav links from home.

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,9 @@
 import Link from "next/link";
 import PageHeader from "@/components/pageheader/PageHeader";
+import home from "@/data/home.json";
+import style from "@/app/home.module.css";
 
-const { name, position, intro, siteLinks } = {
-  name: "Kenneth Zimny",
-  position: "Frontend Developer",
-  intro:
-    "A versatile developer specializing in frontend, full-stack, and embedded systems, I have successfully developed and deployed diverse projects, showcasing adaptability and cross-disciplinary expertise. With experience in launching high-security web applications for leading enterprises, I take ownership of high-impact features, enhancing user experience and meeting business objectives. Additionally, I hold CompTIA Security+ and AWS Cloud Practitioner certifications, and have thrived in various roles, including as a sole developer, business owner, and team member.",
-  siteLinks: [
-    { href: "/works", title: "Works" },
-    { href: "/skills", title: "Skills" },
-    { href: "/contact", title: "Contact" },
-  ],
-};
+const { name, position, intro, siteLinks } = home;
 
 export default function Home() {
   return (
@@ -20,7 +12,7 @@ export default function Home() {
 
       <p>{intro}</p>
 
-      <ul>
+      <ul id={style["site-links"]}>
         {siteLinks.map((link) => {
           return (
             <li key={link.title}>

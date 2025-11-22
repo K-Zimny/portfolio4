de71001e1bb857ee0b1847d0c3eb042cd372fa7e
Ken Zimny <kenneth.zimny@gmail.com>
2025-11-22 15:52:17 -0600

    Adds theming component

--- src/components/pageheader/PageHeader.jsx
+++ src/components/pageheader/PageHeader.jsx
@@ -1,10 +1,12 @@
 import style from "@/components/pageheader/pageheader.module.css";
+import Accent from "../theme/Accent";
 
 export default function PageHeader({ title, subtitle, className }) {
   return (
     <header className={`${style.header} ${className}`}>
       <h1 className={style.title}>{title}</h1>
       {subtitle && <p className={style.subtitle}>{subtitle}</p>}
+      <Accent dataType="desktop" />
     </header>
   );
 }
